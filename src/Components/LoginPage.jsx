103d1e7dbe15bebe9de8e402dff44370de59640c
selvabalaji <selvabalaji.c@outlook.com>
2025-01-07 12:57:08 +0530

    final commit

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -3,30 +3,42 @@ import cauroselImg from '../assets/10526367.jpg'
 function LoginPage() {
     return (
         <>
-            <nav>
-                <div id='navbar'>
-                    <h1>Recyclick</h1>
+            <div id='login-page'>
+                <nav>
+                    <div id='navbar'>
+                        <h1>Recyclick</h1>
+                    </div>
+                </nav>
+                <div id='caurosel-img'>
+                    <img src={cauroselImg} width='70%' height='500px' alt="" />
                 </div>
-            </nav>
-            <div id='caurosel-img'>
-                <img src={cauroselImg} width='70%' height='500px' alt="" />
-            </div>
-            <div id='login-content'>
-                <div>
-                    <h1>Rearn Rewards For e-waste recycling</h1>
-                </div>
-                <div>
-                    <p>Get paid to recycle your used electronics.it's free and easy to start</p>
-                </div>
-                <div>
-                    <button id='signup-btn' >Sign Up</button>
-                </div>
-                <div>
-                    <button id='login-btn'>Log in</button>
-                </div>
+                <div id='login-content'>
+                    <div>
+                        <h1>Rearn Rewards For e-waste recycling</h1>
+                    </div>
+                    <div>
+                        <p>Get paid to recycle your used electronics.it's free and easy to start</p>
+                    </div>
+                    <div>
+                        <button
+                            onClick={() => {
+                                var loginPage = document.getElementById('login-page')
+                                var rewardPage = document.getElementById('rewards-page')
+                                loginPage.style.display = 'none'
+                                rewardPage.style.display = 'block'
+                                console.log('working');
 
-                <div>
-                    <p>By signing up,you agree to our Terms and Service and Privacy Policy. </p>
+                            }
+                            }
+                            id='signup-btn' >Sign Up</button>
+                    </div>
+                    <div>
+                        <button id='login-btn'>Log in</button>
+                    </div>
+
+                    <div>
+                        <p>By signing up,you agree to our Terms and Service and Privacy Policy. </p>
+                    </div>
                 </div>
             </div>
         </>
--- src/Components/RewardsPage.jsx
+++ src/Components/RewardsPage.jsx
@@ -1,8 +1,89 @@
 import '../Components/RewardsPage.css'
+import giftimage from '../assets/gift.png'
+import image3 from '../assets/gift-card-two.jpg'
+import Rewards from '../assets/trophy.png'
+import Rewards2 from '../assets/trophy 2.png'
+import tag from '../assets/tag.png'
+import find from '../assets/find.png'
+import account from '../assets/account.png'
+
+
 function RewardsPage() {
     return (
         <>
-            <p>hiii</p>
+            <div id='rewards-page'>
+                <div>
+                    <div id='nav'>
+                        <h1>Recycle for Rewards</h1>
+                    </div>
+                    <div id='gift-content'>
+                        <div id='gift-content-1'>
+                            <img src={giftimage} width='25px' alt="" />
+                            <h3>Earn Points</h3>
+                            <p>Get points for recycling your e-waste</p>
+                        </div>
+                        <div id='gift-content-1'>
+                            <img src={giftimage} width='25px' alt="" />
+                            <h3>Redeem Points</h3>
+                            <p>Used points to Redeem gift cards</p>
+                        </div>
+                    </div>
+
+                    <div id='button'>
+                        <button>Shedule a Pickup</button>
+                    </div>
+
+
+                </div>
+                <div>
+                    <div >
+                        <h1 id='offer'>Offers</h1>
+                    </div>
+                    <div id='gift-card-detail'>
+                        <div>
+                            <img src={image3} width='200px' alt="" />
+                            <h6>$20 Amazon Gift Card</h6>
+                        </div>
+                        <div>
+                            <img src={image3} width='200px' alt="" />
+                            <h6>$25 Amazon Gift Card</h6>
+                        </div>
+                        <div>
+                            <img src={image3} width='200px' alt="" />
+                            <h6>$50 Amazon Gift Card</h6>
+                        </div>
+
+                    </div>
+                </div>
+
+                <footer>
+                    <div>
+                        <img id='reward-img'
+                            onMouseOver={() => {
+                                var reward = document.getElementById('reward-img')
+                                reward.src = '/src/assets/trophy.png'
+                                onmouseleave(() => {
+                                    reward.src = ' /src/assets/trophy%202.png'
+                                })
+                            }}
+                            src={Rewards2} width='30px' alt="" />
+                        <p>Rewards</p>
+                    </div>
+                    <div>
+                        <img src={tag} width='30px' alt="" />
+                        <p>Offers</p>
+                    </div>
+                    <div>
+                        <img src={find} width='30px' alt="" />
+                        <p>How it works</p>
+                    </div>
+                    <div>
+                        <img src={account} width='30px' alt="" />
+                        <p>Account</p>
+                    </div>
+                </footer>
+
+            </div>
         </>
     );
 }
